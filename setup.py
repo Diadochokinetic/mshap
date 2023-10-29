5780af7745a9cf5169603e92316e501c3d9beb88
Diadochokinetic <diadochokinetic@gmail.com>
2023-10-29 19:43:55 +0100

    Clean Up R code and add requirements.

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mshap",
-    version="0.0.1",
+    version="0.1.0",
     author="Diadochokinetic",
     author_email="diadochokinetic@googlemail.com",
-    description="A Python port of mshap two interpret combined model outputs.",
+    description="A Python port of R package mshap to interpret combined model outputs.",
     python_requires=">=3.10",
     long_description=long_description,
     packages=setuptools.find_packages(),
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(mshap)
-
-test_check("mshap")
